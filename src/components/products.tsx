58d86946609791433dc4834e1be1e4487ca8fddb
Gauri Paudel <gauri.paudeln@gmail.comm>
2021-06-08 07:24:24 -0500

    expot to excel

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import '@progress/kendo-theme-default/dist/all.css';
-import { Grid, GridColumn, GridDataStateChangeEvent, GridRowClickEvent } from "@progress/kendo-react-grid";
+import { Grid, GridColumn, GridDataStateChangeEvent, GridRowClickEvent, GridToolbar } from "@progress/kendo-react-grid";
 import { process, State } from "@progress/kendo-data-query";
 import { ProductsProps } from "src/models/products-props";
-
+import { ExcelExport } from '@progress/kendo-react-excel-export';
 
 function Products(props :ProductsProps) {
-  
-      return (<Grid
+      let _export: ExcelExport | null;
+
+        const excelExport = () => {
+          if(_export!==null)
+             _export.save();
+        };
+      return (
+        
+      <ExcelExport data={props.data} ref={(exporter) => (_export = exporter)}>    
+      <Grid
         data={process(props.data, props.gridDataState)}
         pageable={true}
         sortable={true}
@@ -16,12 +24,24 @@ function Products(props :ProductsProps) {
         style={{ height: "400px" }}
         onRowClick={props.onGridRowClick}
       >
+        <GridToolbar>
+          <button
+            title="Export Excel"
+            className="k-button k-primary"
+            onClick={excelExport}
+          >
+            Export to Excel
+          </button>
+        </GridToolbar>
+
         {
         props.gridColumns.map(c=><GridColumn field={c.field} title={c.title} format={c.format}  key={c.field}/>)
         }
         
         
-      </Grid>);
+      </Grid>
+      </ExcelExport>
+      );
 
 
 }
